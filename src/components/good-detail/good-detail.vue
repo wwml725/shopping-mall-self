a53e7426ca2172f234bce7f84736cadaa80a1b13
wwml725 <wwml725@163.com>
2020-06-14 12:54:22 +0800

    all

---
+++ src/components/good-detail/good-detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="good-detail-wrapper">
+    {{title}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "good-detail",
+    data() {
+      return {
+        title: 'good-detail'
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {},
+    computed: {},
+    watch: {},
+    components: {}
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
